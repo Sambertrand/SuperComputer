6c3f4d662329be3d4d5d8143876f8d9c5f5bbf38
Martin Degeldt <15076@ecam.be>
2017-12-09 19:21:13 +0100

    Add Polynomes Class library

---
+++ Polynome/Polynome/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperComputer;
+
+namespace Polynome
+{
+    public class Polynome
+    {
+        private double[] coeffs;
+
+        public Polynome(double[] coeffs)
+        {
+            this.coeffs = coeffs;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            int i = 0;
+            foreach(double coeff in coeffs)
+            {
+                if (coeff != 0)
+                {
+                    if (i == 0)
+                    {
+                        result = coeff.ToString();
+                    }
+                    else if (i == 1)
+                    {
+                        result = coeff.ToString() +"x" + result;
+                    }
+                    else
+                    {
+                        result = coeff.ToString() + "x^" + i.ToString()+ " " + result;
+                    }
+                    if (i+1 != coeffs.Count() && coeff > 0)
+                    {
+                        result = " +" + result;
+                    }
+                }
+                i++;
+               
+            }
+            return result;
+        }
+    }
+}
