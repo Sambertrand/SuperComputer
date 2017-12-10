2e9953ae25ef968354be31e76b35bdf416644843
Sam Bertrand <15027@ecam.be>
2017-12-10 18:52:10 +0100

    stats project adding

---
+++ Stats/Stats/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stats
+{
+    public class Stats
+    {
+        private double[];
+
+        public 
+    }
+}
