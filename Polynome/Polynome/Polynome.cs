2746d1f644c28c43b43283acadf897903d8f4e3c
Martin Degeldt <15076@ecam.be>
2017-12-10 20:11:00 +0100

    Add + method

--- Polynome/Polynome/Polynome.cs
+++ Polynome/Polynome/Polynome.cs
@@ -10,10 +10,15 @@ namespace Polynome
     public class Polynome
     {
         private double[] coeffs;
+        private int degree;
 
         public Polynome(double[] coeffs)
         {
+            while (coeffs.Last() == 0)
+                coeffs = coeffs.Take(coeffs.Count() - 1).ToArray();
+
             this.coeffs = coeffs;
+            degree = this.coeffs.Length - 1;
         }
 
         public override string ToString()
@@ -25,26 +30,65 @@ namespace Polynome
                 if (coeff != 0)
                 {
                     if (i == 0)
-                    {
                         result = coeff.ToString();
-                    }
+
                     else if (i == 1)
-                    {
                         result = coeff.ToString() +"x" + result;
-                    }
+
                     else
-                    {
                         result = coeff.ToString() + "x^" + i.ToString() + result;
-                    }
-                    if (i+1 != coeffs.Count() && coeff > 0)
-                    {
+
+                    if (i + 1 < coeffs.Length && coeff > 0)
                         result = "+" + result;
-                    }
                 }
+                
                 i++;
                
             }
+            if (result == "")
+                result = "0";
+
             return result;
+        }
+
+        public static Polynome operator + (Polynome left, Polynome right)
+        {
+           
+            Polynome smaller;
+            Polynome bigger;
+            int i = 0;
+            if (left.Degree < right.Degree)
+            {
+                smaller = left;
+                bigger = right;
+            }
+            else
+            {
+                smaller = right;
+                bigger = left;
+            }
+            double[] coef = new double[bigger.Degree + 1];
+            while (i <= smaller.Degree)
+            {
+                coef[i] = smaller.Coeffs[i] + bigger.Coeffs[i];
+                i++;
+            }
+            while (i <= bigger.Degree)
+            {
+                coef[i] = bigger.Coeffs[i];
+                i++;
+            }
+            return new Polynome(coef);
+        }
+
+        public int Degree
+        {
+            get { return degree; }
+        }
+
+        public double[] Coeffs
+        {
+            get { return coeffs; }
         }
     }
 }
--- Polynome/Polynome/Test.cs
+++ Polynome/Polynome/Test.cs
@@ -11,27 +11,50 @@ namespace Polynome
     [TestFixture()]
     public class TestPolynome
     {
-       
-        private double[] coef1 = {1, 2, 3 };
+
+        private double[] coef1 = { 1, 2, 3, 0 };
         Polynome pol1;
         private double[] coef2 = { 1, -2, -3 };
         Polynome pol2;
+        private double[] coef3 = { 0, -3 };
+        Polynome pol3;
+        private double[] coef4 = {2, 0};
+        Polynome pol4;
+        Polynome polP;
 
         [SetUp()]
         public void Init()
         {
             pol1 = new Polynome(coef1);
             pol2 = new Polynome(coef2);
+            pol3 = new Polynome(coef3);
+            pol4 = new Polynome(coef4);
         }
 
 
         [Test()]
-        public void TestwriteName()
+        public void TestwritePol()
         {
             Assert.AreEqual("3x^2+2x+1", pol1.ToString());
             Assert.AreEqual("-3x^2-2x+1", pol2.ToString());
+            Assert.AreEqual("-3x", pol3.ToString());
+            Assert.AreEqual("2", pol4.ToString());
         }
-        
 
+        [Test()]
+        public void TestPolDegree()
+        {
+            Assert.AreEqual(2, pol1.Degree);
+            Assert.AreEqual(1, pol3.Degree);
+        }
+
+        [Test()]
+        public void TestPolAdd()
+        {
+            polP = pol1 + pol2;
+            Assert.AreEqual("2", polP.ToString());
+            polP = pol1 + pol3 + pol4;
+            Assert.AreEqual("3x^2-1x+3", polP.ToString());
+        }
     }
 }
