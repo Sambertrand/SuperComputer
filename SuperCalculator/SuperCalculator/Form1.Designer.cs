808578325ade4cfe0efc00ff7c2063b845f0dfa2
Sam Bertrand <15027@ecam.be>
2017-12-13 21:19:31 +0100

    new form completed

--- SuperCalculator/SuperCalculator/Form1.Designer.cs
+++ SuperCalculator/SuperCalculator/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Compute = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Help = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Compute
+            // 
+            this.Compute.Location = new System.Drawing.Point(839, 542);
+            this.Compute.Name = "Compute";
+            this.Compute.Size = new System.Drawing.Size(124, 52);
+            this.Compute.TabIndex = 0;
+            this.Compute.Text = "Compute";
+            this.Compute.UseVisualStyleBackColor = true;
+            this.Compute.Click += new System.EventHandler(this.Compute_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(839, 70);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(124, 35);
+            this.Load.TabIndex = 1;
+            this.Load.Text = "Load Function";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(839, 500);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(124, 36);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Help
+            // 
+            this.Help.Location = new System.Drawing.Point(839, 12);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(124, 52);
+            this.Help.TabIndex = 3;
+            this.Help.Text = "Help";
+            this.Help.UseVisualStyleBackColor = true;
+            this.Help.Click += new System.EventHandler(this.Help_Click_1);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(839, 600);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(124, 38);
+            this.Save.TabIndex = 4;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Output.Location = new System.Drawing.Point(12, 12);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(821, 524);
+            this.Output.TabIndex = 5;
+            this.Output.Text = "";
+            this.Output.TextChanged += new System.EventHandler(this.Ouput_TextChanged);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 542);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(821, 96);
+            this.Input.TabIndex = 4;
+            this.Input.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(975, 650);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Compute);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Compute;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.RichTextBox Input;
     }
 }
 
--- SuperCalculator/SuperCalculator/Form1.cs
+++ SuperCalculator/SuperCalculator/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SuperComputer;
+using System.IO;
 
 namespace SuperCalculator
 {
@@ -17,6 +18,72 @@ namespace SuperCalculator
         {
             //IFunction function = Program.LoadFunction(path);
             InitializeComponent();
+        }
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //va revoyer un 2eme form qui afiche chaque help d'afilé dans la text box
+        private void Help_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        //loads new functions
+        private void Load_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "dll files (*.dll)|*.dll|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            Output.Text = openFileDialog.FileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        //clears the output
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Output.Text = "";
+        }
+
+        //gl
+        private void Compute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //save function
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string[] lines = Output.Text.Split('\n');
+            System.IO.File.WriteAllLines(@"../../../computed.txt", lines);
+            MessageBox.Show("Operations Saved!");
+        }
+
+        //output text
+        private void Ouput_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
