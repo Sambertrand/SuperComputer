8f11beb9a56a1102546cc40df0511860a329bce3
Sam Bertrand <15027@ecam.be>
2017-12-13 20:21:41 +0100

    sam2 merge de fortune

---
+++ Average/Average/Average.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperComputer;
+
+namespace Average
+{
+    class Average : Function<double>
+    {
+        private string name = "Average";
+        public string Name
+        {
+            get { return name; }
+        }
+
+        private string help = "Average(serie de nombre divisés par un espace";
+        public string HelpMessage
+        {
+            get { return help; }
+        }
+        private string[] paramname = { "une pool de nombres" };
+        public string[] ParametersName
+        {
+            get { return paramname; }
+        }
+
+        public double Evaluate(string[] args)
+        {
+            double[] pool = Array.ConvertAll(args, double.Parse);
+            return pool.Average();
+        }
+    }
+}
+
