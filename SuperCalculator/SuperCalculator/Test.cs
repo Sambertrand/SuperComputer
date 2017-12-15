fc86d8acc1e7990a616539a89b9915c2beb736d2
Martin Degeldt <15076@ecam.be>
2017-12-15 14:31:40 +0100

    finish tests

--- SuperCalculator/SuperCalculator/Test.cs
+++ SuperCalculator/SuperCalculator/Test.cs
@@ -22,16 +22,46 @@ namespace SuperCalculator
             input = new string[]
             {
                 "coucou",
+                "coucou 3 4",
                 "Average",
+                "Average superman",
+                "Average 1 2 3 4 5 6",
+                "Derivate",
+                "Derivate superman",
+                "Derivate 1 2 4 3",
+                "Derivate 0",
+                "Roots",
+                "Roots superman",
                 "Roots 4",
-                "Roots 2 0"
+                "Roots 2 0",
+                "StandartDeviation",
+                "StandartDeviation superman",
+                "StandartDeviation 1 2 3 4 5 6",
+                "Variance",
+                "Variance superman",
+                "Variance 1 2 3 4 5 6"
             };
             output = new string[]
             {
                 "Unkwon command",
+                "Unkwon command",
+                "Wrong parameters",
+                "Wrong parameters",
+                "3.5",
+                "Wrong parameters",
+                "Wrong parameters",
+                "3x^2+4x+4",
+                "0",
+                "Wrong parameters",
                 "Wrong parameters",
                 "[None]",
-                "[0]"
+                "[0]",
+                "Wrong parameters",
+                "Wrong parameters",
+                Math.Sqrt(3.5).ToString(),
+                "Wrong parameters",
+                "Wrong parameters",
+                "3.5"
             };
         }
 
@@ -39,9 +69,7 @@ namespace SuperCalculator
         public void TestCalcul()
         {
             for (int i = 0; i < input.Count(); i++)
-            {
-            Assert.AreEqual(output[i], test.GetCalcul(input[i]));
-            }
+                Assert.AreEqual(output[i], test.GetCalcul(input[i]));
         }
     }
 }
